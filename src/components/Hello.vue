0afa2197e6998cd1cba102920d32bd8b90a39def
Kris_Chan <743628145@qq.com>
2017-03-09 22:33:23 +0800

    1.test;

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ test }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -25,7 +25,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      test: '我喜欢你'
     }
   }
 }
