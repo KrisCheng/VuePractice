697988b7d61f9aacce10ede2c477543e04691be9
Kris_Chan <743628145@qq.com>
2017-04-10 20:07:13 +0800

    1.file structure modify;

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,44 +1,45 @@
 <template>
   <transition name="move">
-  <div v-show="showFlag" class="food" id="food">
-    <div class="food-content">
-      <div class="image-header">
-        <img :src="food.image">
-        <div class="back">
-          <i class="el-icon-arrow-left" @click="hide"></i>
+    <div v-show="showFlag" class="food" id="food">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back">
+            <i class="el-icon-arrow-left" @click="hide"></i>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now"><span class="tag">$</span>{{food.price}}</span>
+            <span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
+          </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="add">
+            <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">
+              加入购物车
+            </div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品介绍</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                        :ratings="food.ratings"></ratingselect>
         </div>
       </div>
-      <div class="content">
-        <h1 class="title">{{food.name}}</h1>
-        <div class="detail">
-          <span class="sell-count">月售{{food.sellCount}}份</span>
-          <span class="rating">好评率{{food.rating}}%</span>
-        </div>
-        <div class="price">
-          <span class="now"><span class="tag">$</span>{{food.price}}</span>
-          <span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
-        </div>
-      <div class="cartcontrol-wrapper">
-        <cartcontrol :food="food"></cartcontrol>
-      </div>
-      <transition name="add">
-      <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">
-        加入购物车
-      </div>
-      </transition>
     </div>
-    <split v-show="food.info"></split>
-    <div class="info" v-show="food.info">
-      <h1 class="title">商品介绍</h1>
-      <p class="text">{{food.info}}</p>
-    </div>
-      <split></split>
-      <div class="rating">
-        <h1 class="title">商品评价</h1>
-        <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
-      </div>
-  </div>
-  </div>
   </transition>
 </template>
 
@@ -54,31 +55,31 @@
   const ALL = 2;
 
   export default {
-    props:{
-      food:{
+    props: {
+      food: {
         type: Object
       }
     },
     data(){
-      return{
+      return {
         showFlag: false,
         selectType: ALL,
         onlyContent: true,
-        desc:{
-          all:'全部',
-          positive:'推荐',
-          negative:'吐槽'
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
         }
       };
     },
-    methods:{
+    methods: {
       show(){
         this.showFlag = true;
         this.selectType = ALL;
         this.onlyContent = true;
         this.$nextTick(() => {
-          if(!this.scroll){
-            this.scroll = new BScroll(document.getElementById('food'),{
+          if (!this.scroll) {
+            this.scroll = new BScroll(document.getElementById('food'), {
               click: true
             });
           }
@@ -88,13 +89,13 @@
         this.showFlag = false;
       },
       addFirst(event){
-        if(!event._constructed){
+        if (!event._constructed) {
           return;
         }
-        Vue.set(this.food,'count',1);
+        Vue.set(this.food, 'count', 1);
       }
     },
-    components:{
+    components: {
       cartcontrol,
       split,
       ratingselect
